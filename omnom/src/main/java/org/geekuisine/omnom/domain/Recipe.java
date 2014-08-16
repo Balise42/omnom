b25c64457a7039f5654b31634ef9c2431f994ae6
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2014-08-16 20:41:22 +0200

    Domain class for a recipe

---
+++ omnom/src/main/java/org/geekuisine/omnom/domain/Recipe.java
@@ -0,0 +1,78 @@
+package org.geekuisine.omnom.domain;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Recipe {
+	String name;
+	Map<Ingredient, Quantity> ingredients;
+	int numPersons;
+	Quantity cookingTime;
+	Quantity prepTime;
+	Quantity restTime;
+	List<String> steps;
+	
+	Recipe(){
+		super();
+		ingredients = new HashMap<Ingredient,Quantity>();
+		steps = new ArrayList<String>();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Map<Ingredient, Quantity> getIngredients() {
+		return ingredients;
+	}
+
+	public void setIngredients(Map<Ingredient, Quantity> ingredients) {
+		this.ingredients = ingredients;
+	}
+
+	public int getNumPersons() {
+		return numPersons;
+	}
+
+	public void setNumPersons(int numPersons) {
+		this.numPersons = numPersons;
+	}
+
+	public Quantity getCookingTime() {
+		return cookingTime;
+	}
+
+	public void setCookingTime(Quantity cookingTime) {
+		this.cookingTime = cookingTime;
+	}
+
+	public Quantity getPrepTime() {
+		return prepTime;
+	}
+
+	public void setPrepTime(Quantity prepTime) {
+		this.prepTime = prepTime;
+	}
+
+	public Quantity getRestTime() {
+		return restTime;
+	}
+
+	public void setRestTime(Quantity restTime) {
+		this.restTime = restTime;
+	}
+
+	public List<String> getSteps() {
+		return steps;
+	}
+
+	public void setSteps(List<String> steps) {
+		this.steps = steps;
+	}
+}
