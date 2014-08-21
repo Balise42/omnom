aa30a8f7c47004112d60eacb44c890fa3b70e6a7
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2014-08-21 05:44:17 +0200

    Added simple service for recipes

---
+++ omnom/src/main/java/org/geekuisine/omnom/service/RecipeService.java
@@ -0,0 +1,11 @@
+package org.geekuisine.omnom.service;
+
+import java.util.List;
+
+import org.geekuisine.omnom.domain.Recipe;
+
+public interface RecipeService {
+	public Recipe getRecipeById(int recipeId);
+	public List<Recipe> getAllRecipes();
+
+}
---
+++ omnom/src/main/java/org/geekuisine/omnom/service/impl/RecipeServiceImpl.java
@@ -0,0 +1,26 @@
+package org.geekuisine.omnom.service.impl;
+
+import java.util.List;
+
+import org.geekuisine.omnom.domain.Recipe;
+import org.geekuisine.omnom.repository.RecipeRepository;
+import org.geekuisine.omnom.service.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RecipeServiceImpl implements RecipeService {
+	@Autowired
+	private RecipeRepository recipeRepository;
+
+	@Override
+	public Recipe getRecipeById(int recipeId) {
+		return recipeRepository.getRecipeById(recipeId);
+	}
+
+	@Override
+	public List<Recipe> getAllRecipes() {
+		return recipeRepository.getAllRecipes();
+	} 
+
+}
